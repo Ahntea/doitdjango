d6a795f8a22983e75e0f243374d8b1cb71e57bab
ahntea-admin <dig4095@gmail.com>
2021-12-12 16:40:48 +0900

    CVB로 블로그 포스트 목록 페이지 만들기

--- blog/views fbv.py
+++ blog/views fbv.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
 # Create your views here.
 
-class PostList(ListView):
-    model = Post
-    ordering = '-pk'
-    template_name = 'blog/index.html'
 
+def index(request):
+    p = Post.objects.all().order_by('-pk')
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts':p,
+        }
+    )
+    
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
     
