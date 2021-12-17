942764c4bafd13e12849e772e56d634944dce6b5
ahntea-admin <dig4095@gmail.com>
2021-12-17 21:25:15 +0900

    카테고리 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -6,13 +6,17 @@ import os
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
-    
+
     class Meta:
         verbose_name_plural = 'Categories'
     
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return f'/blog/category/{self.slug}/'
+    
+    
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
