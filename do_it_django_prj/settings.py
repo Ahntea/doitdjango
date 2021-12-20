f8dff6c026056cc46301a8669f1b75a13f7dffe9
ahntea-admin <dig4095@gmail.com>
2021-12-20 23:34:43 +0900

    Content 입력란에 마크다운 적용하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'markdownx',
     'crispy_forms',
     'blog',
     'single_pages',
@@ -128,3 +129,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('markdownx/', include('markdownx.urls')),
     path('blog/', include('blog.urls')),
     path('', include('single_pages.urls')),
     path('admin/', admin.site.urls),
