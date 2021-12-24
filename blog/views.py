2a1bc25dd727e6e3e58b3c354c10eef070a48b38
ahntea-admin <dig4095@gmail.com>
2021-12-24 11:07:47 +0900

    댓글 수정 기능 추가하기

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic.edit import UpdateView
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import redirect, render, get_object_or_404
@@ -153,6 +153,16 @@ def new_comment(request, pk):
             return redirect(post.get_absolute_url())
     else:
         raise PermissionDenied
+    
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+    
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user==self.get_object().author:
+            return super(CommentUpdate,self).dispatch(request,*args,**kwargs)
+        else:
+            raise PermissionDenied
 ###### 카카오
 
 def kakao_login(request):
