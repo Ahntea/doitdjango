5df4cd67f764f00a544bd22d9d568c20f102320e
ahntea-admin <dig4095@gmail.com>
2022-01-08 20:58:00 +0900

    개발용과 배포용 도커 파일 분리하기

--- do_it_django_prj/settings.py
+++ do_it_django_prj/settings.py
@@ -29,7 +29,8 @@ with open(secret_file) as f:
 SECRET_KEY = secrets["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = int(os.environ.get('DEBUG',1))
+# DEBUG = int(os.environ.get('DEBUG',1))
+DEBUG = os.environ.get('DEBUG',1)
 
 # if os.environ.get('DJANGO_ALLOWED_HOSTS'):
 #     ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS'.split(' '))
